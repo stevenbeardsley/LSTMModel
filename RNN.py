5b12aa1a2b1512541c67136dda2b6fdf79c6686b
MattBeardsley <mfbeardsley2004@gmail.com>
2024-10-11 19:12:19 +0100

    entered the nornalisation stuff

--- RNN.py
+++ RNN.py
@@ -15,11 +15,17 @@ def GetDataSet():
     print (data)
     #Create CNN
     print ("this is each bit in 5 days:")
-    priceList=  data['Adj Close']
+    priceList=  data['Target']
     dateList  = data.index
+    print(priceList)
+    return priceList
+    #Error is that in getdataset there isn't a header called Target.
     
 #This Function is to take 5 days worth of reference price, and the next day (target) and return them 
-def FormatData(dataset):
+def FormatData(data):
+    print(type(data))
+    data = data.to_frame()
+    data.columns = ['Target'] + list(data.columns[1:])
     data["Target"] = (data["Target"] - data["Target"].min())/(data["Target"].max() - data["Target"].min())
     price_list = list(data['Target'])
     print(price_list[0])
@@ -28,15 +34,19 @@ def FormatData(dataset):
     for date in dataset["Target"]:
         prices.append(date)
     #** With this list iterate over it picking five elements and a 'target element'and put it into another function called Calculate****
+    sample = prices[:4]
+    target = prices[5]
+    return sample, target
 
 def CalculateTarget(data, target):
     targetGuess = 0
 
     return targetGuess
+
 def main():
-    GetDataSet()
-    FormatData()#Return the list of 5 and the target, assigned to two variables data, target
-    ##Add a function call to calculate target with the 5 days of data list and target 
-
-
+    priceList = GetDataSet()
+    sample, target = FormatData(priceList)
+    print(sample, target)
+    print(data.columns)
+main()
 #Description- This project uses the Long Short Term Memory (LSTM) algorithm to weight short term fluctuations in changes in stock prices against long term trends. This model incorporates neural networks and machine learning to accurately use past data to predict future stock prices.

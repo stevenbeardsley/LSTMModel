8c913d11b46e4ffb25e1a008b5ca898d36f44939
Steven Beardsley <swbeardsley2004@gmail.com>
2024-10-13 14:10:44 +0100

    Up to format data

--- RNN.py
+++ RNN.py
@@ -1,4 +1,5 @@
 import tensorflow as tf 
+import numpy as np
 from tensorflow.keras import Sequential 
 from tensorflow.keras.layers import Dense
 import yfinance as yf 
@@ -11,32 +12,25 @@ def GetDataSet():
     data['Target']  = data['Adj Close']- data.Open
     data['Target'] = data['Target'] .shift(-1)
 
-    print ("This is the data ")
-    print (data)
     #Create CNN
-    print ("this is each bit in 5 days:")
     priceList=  data['Target']
     dateList  = data.index
-    print(priceList)
-    return priceList
-    #Error is that in getdataset there isn't a header called Target.
+    return priceList 
+   #Error is that in getdataset there isn't a header called Target.
     
 #This Function is to take 5 days worth of reference price, and the next day (target) and return them 
-def FormatData(data):
-    print(type(data))
+def FormatData(data, sequenceLength):
     data = data.to_frame()
     data.columns = ['Target'] + list(data.columns[1:])
     data["Target"] = (data["Target"] - data["Target"].min())/(data["Target"].max() - data["Target"].min())
-    price_list = list(data['Target'])
-    print(price_list[0])
-    print(data["Target"])
-    prices = []
-    for date in dataset["Target"]:
-        prices.append(date)
-    #** With this list iterate over it picking five elements and a 'target element'and put it into another function called Calculate****
-    sample = prices[:4]
-    target = prices[5]
-    return sample, target
+    priceList = list(data['Target'])
+    sampleList, targetList = [],[]
+    for x in range (len(priceList) - sequenceLength):
+        sampleList.append(priceList[x:x+sequenceLength])
+        targetList.append(x+sequenceLength)
+    sampleList = np.array(sampleList)
+    targetList = np.array(targetList)
+    return sampleList, targetList
 
 def CalculateTarget(data, target):
     targetGuess = 0
@@ -44,9 +38,9 @@ def CalculateTarget(data, target):
     return targetGuess
 
 def main():
+    sequenceLength = 10
     priceList = GetDataSet()
-    sample, target = FormatData(priceList)
-    print(sample, target)
-    print(data.columns)
+    sampleList, targetList = FormatData(priceList, sequenceLength)
+    
 main()
 #Description- This project uses the Long Short Term Memory (LSTM) algorithm to weight short term fluctuations in changes in stock prices against long term trends. This model incorporates neural networks and machine learning to accurately use past data to predict future stock prices.
